711617ad5d11dfe5c8e8db9a042270e4da1a1fc8
adirsinai <adirsinai@gmail.com>
2021-08-30 18:58:53 +0300

    first commit

---
+++ counter-timer/script.js
@@ -0,0 +1,28 @@
+const daysEl = document.querySelector("#days");
+const hoursEl = document.querySelector("#hours");
+const minsEl = document.querySelector("#mins");
+const secondsEl = document.querySelector("#seconds");
+
+const birthDay = "8 Jan 2022";
+
+function countdown() {
+  const birthDayDate = new Date(birthDay);
+  const currentDate = new Date();
+
+  const totalSeconds = (birthDayDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds % 60);
+
+  console.log(days, hours, minutes, seconds);
+  daysEl.textContent = days;
+  hoursEl.textContent = hours;
+  minsEl.textContent = minutes;
+  secondsEl.textContent = seconds;
+}
+
+//init call
+countdown();
+
+setInterval(countdown, 1000);
